73d414082330bbdc7dc0e251fb684b03b6df85d1
renatolaranja <matthew.b.geiger@gmail.com>
2019-01-14 22:23:31 -0500

    added HTLM layout and frebase link

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB9Atswe7ksuqnYEqcrHDJoKcKY29fyNFA",
+    authDomain: "hw7firebase-50b3f.firebaseapp.com",
+    databaseURL: "https://hw7firebase-50b3f.firebaseio.com",
+    projectId: "hw7firebase-50b3f",
+    storageBucket: "hw7firebase-50b3f.appspot.com",
+    messagingSenderId: "890938952176"
+  };
+  firebase.initializeApp(config);
